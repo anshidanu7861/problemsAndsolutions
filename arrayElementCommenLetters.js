70699c5d57e3ad765f0062cb47976ed67e52ea53
anshid <anshidanu99@gmail.com>
2024-07-06 12:39:30 +0530

    add

--- arrayElementCommenLetters.js
+++ arrayElementCommenLetters.js
@@ -1,11 +1,18 @@
+// find array inside common letters
+
 const array = ["flower", "float", "flirt"];
 
 let sortedArr = array.sort();
 console.log(sortedArr);
 
 let lastStr = sortedArr[sortedArr.length - 1];
-let str = '';
-for (let i = 0; i < (sortedArr[0].length < lastStr.length ? sortedArr[0].length: lastStr.length); i++) {
+let str = "";
+for (
+  let i = 0;
+  i <
+  (sortedArr[0].length < lastStr.length ? sortedArr[0].length : lastStr.length);
+  i++
+) {
   if (sortedArr[0][i] === lastStr[i]) {
     str += lastStr[i];
   } else {
